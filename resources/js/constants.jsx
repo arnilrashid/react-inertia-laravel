9cf3508f8d6935fef03da0981f12b9786088b006
Md Mamun Ar Rashid <134777056+arnilrashid@users.noreply.github.com>
2024-12-18 13:06:15 +0600

    first commit

---
+++ resources/js/constants.jsx
@@ -0,0 +1,31 @@
+export const PROJECT_STATUS_CLASS_MAP = {
+  pending: "bg-gradient-to-r from-rose-600 to-rose-400",
+  in_progress: "bg-gradient-to-r from-fuchsia-600 to-fuchsia-400",
+  completed: "bg-gradient-to-r from-emerald-600 to-emerald-400",
+};
+export const PROJECT_STATUS_TEXT_MAP = {
+  pending: "Pending",
+  in_progress: "In Progress",
+  completed: "Completed",
+};
+export const TASK_STATUS_CLASS_MAP = {
+  pending: "bg-amber-500",
+  in_progress: "bg-blue-500",
+  completed: "bg-green-500",
+};
+export const TASK_STATUS_TEXT_MAP = {
+  pending: "Pending",
+  in_progress: "In Progress",
+  completed: "Completed",
+};
+export const TASK_PRIORITY_CLASS_MAP = {
+  low: "bg-teal-500",
+  medium: "bg-yellow-500",
+  high: "bg-red-500",
+};
+
+export const TASK_PRIORITY_TEXT_MAP = {
+  low: "Low",
+  medium: "Medium",
+  high: "High",
+};
